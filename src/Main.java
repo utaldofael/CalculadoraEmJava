9c38123e91e005a38ef53935d096d24a35a70ac3
raphael <raphaelaugustob1509@gmail.com>
2026-04-23 19:17:42 -0300

    Feito a calculadora inteira na classe Main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main{
   public static void main(String[] args) throws RuntimeException {
     try {
@@ -8,7 +11,76 @@ public class Main{
         System.out.println("Version: 1.0");
       }
     } catch (Exception ignored){}
-    System.out.println("Calculadora Em Java");
-    System.out.println("Programa executado!");
+
+    Scanner scan = new Scanner(System.in);
+    ArrayList<String> resultado = new ArrayList<>();
+    int i = 0;
+    int num;
+    int res = 0;
+    String sair;
+    String opp;
+
+    System.out.println("=====Calculadora Em Java=====");
+    System.out.println("Operadores: +, -, *, /\n");
+    while (true) {
+      if (i == 0){
+        System.out.print("Informe um valor: ");
+        if (scan.hasNextInt()){
+          res = scan.nextInt();
+          resultado.add(String.valueOf(res));
+          i = 1;
+        } else {
+          System.out.println("Você digitou um valor inválido.");
+          scan.next();
+          continue;
+        }
+
+      }
+      System.out.print("Informe o operador: ");
+      opp = scan.next();
+
+      System.out.print("Informe um valor: ");
+      if (scan.hasNextInt()){
+        num = scan.nextInt();
+      } else {
+        System.out.println("Você digitou um valor inválido.");
+        scan.next();
+        continue;
+      }
+
+      switch (opp){
+        case "+":
+          res += num;
+          break;
+        case "-":
+          res -= num;
+          break;
+        case "*":
+          res *= num;
+          break;
+        case "/":
+          res /= num;
+          break;
+        default:
+          System.out.println("Operador desconhecido...");
+          continue;
+      }
+      resultado.add(opp);
+      resultado.add(String.valueOf(num));
+
+      for (String valor : resultado){
+        System.out.print(valor+" ");
+
+      }
+      System.out.println("= "+res);
+      System.out.print("Aperte ENTER para continuar ou digite \"0\" para sair");
+      sair = scan.nextLine();
+      sair = scan.nextLine();
+      if (sair.equals("0")){
+        System.out.println("Encerrando o programa...");
+        break;
+      }
+    }
+
   }
 }
